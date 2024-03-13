779035908e9b05fa45a4005bbef0b31320282955
Divya <divs115@gmail.com>
2024-03-13 10:49:18 -0700

    Fixed indentation

--- card.php
+++ card.php
@@ -1,5 +1,6 @@
 <?php  
     session_start();
+    $tries = 3;
     if(isset($_SESSION["logged"]) && $_SESSION["logged"] == "yes"){
 ?>
 
@@ -18,7 +19,7 @@
        
     <div class="container d-flex justify-content-center">
       <div class="card my-5 mx-5 " style="width:500px">
-        
+          
           <div id="carousel-bars-imgs" class="carousel slide">
               <div class="carousel-indicators" id="carousel-indicators-imgs"></div>
               <div class="carousel-inner" id="carousel-items-imgs"></div>
@@ -34,11 +35,8 @@
            </div>
            
            <div class="card-body my-3">
-               
             <div class="carousel carousel-dark slide" id="carousel-bars-info">
-              
               <div class="carousel-inner" id="carousel-items-info"></div>
-
               <button class="carousel-control-prev" type="button" data-bs-target="#carousel-bars-info" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
@@ -47,21 +45,22 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
               </button>
+                
            </div>
                <div class="carousel-indicators" id="carousel-indicators-info"></div>
            </div>
         </div>  
     </div>
-      <div class="container d-flex justify-content-center">
-          <a href="#" class="btn btn-primary" style="height: 40px">Submit</a>
-          <h4 class="mx-5 mb-5 text-center" id="tries"> Tries : 3 </h4>
-      </div>
+    <div class="container d-flex justify-content-center">
+      <a href="#" class="btn btn-primary" style="height: 40px">Submit</a>
+      <h4 class="mx-5 mb-5 text-center" id="tries"> Tries : <?= $tries ?></h4>
+    </div>
       
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script type="text/javascript" src="./script.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
 
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let img_links = {
-    "elephant" : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/elephant.png",
-    "willow" : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/willow-sm.jpg",
+    "elephant": "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/elephant.png",
+    "willow"  : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/willow-sm.jpg",
     "axelotl" : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/axelotl.png",
     "cheetah" : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/cheetah.png",
     "giraffe" : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/giraffe.png",
-    "cow" : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/cow.png"
+    "cow"     : "https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/cow.png"
 };
     
 
@@ -18,18 +18,15 @@ let img_info = {
 };
 
 $( document ).ready(function() {
-    
     /******************* carousel images ********************/
     //add carousel indicators (bars on the bottom of the image)
     for(let i = 0; i < 7; i++){
         if(i == 0){
-            $("#carousel-indicators-imgs").append(
-            `
+            $("#carousel-indicators-imgs").append(`
                 <button type="button" data-bs-target="#carousel-bars-imgs" data-bs-slide-to="`+i+`" aria-label="Slide `+(i+1)+`" class="active" aria-current="true"></button>
             `);
         }else{
-            $("#carousel-indicators-imgs").append(
-            `
+            $("#carousel-indicators-imgs").append(`
                 <button type="button" data-bs-target="#carousel-bars-imgs" data-bs-slide-to="`+i+`" aria-label="Slide `+(i+1)+`"></button>
             `);
         }
@@ -41,15 +38,13 @@ $( document ).ready(function() {
     for(let key of keys){
         if(tmp == 0){
             tmp++;
-            $("#carousel-items-imgs").append(
-            `
+            $("#carousel-items-imgs").append(`
                 <div class="carousel-item active" >
                   <img src="`+img_links[key]+`" style="width:500px; height:500px" alt="`+key+`">
                 </div>
             `);
         }else{
-            $("#carousel-items-imgs").append(
-            `
+            $("#carousel-items-imgs").append(`
                 <div class="carousel-item" >
                   <img src="`+img_links[key]+`" style="width:500px; height:500px" alt="`+key+`">
                 </div>
@@ -61,13 +56,11 @@ $( document ).ready(function() {
     //add carousel indicators 
     for(let i = 0; i < 7; i++){
         if(i == 0){
-            $("#carousel-indicators-info").append(
-            `
+            $("#carousel-indicators-info").append(`
                 <button type="button" data-bs-target="#carousel-bars-info" data-bs-slide-to="`+i+`" aria-label="Slide `+(i+1)+`" class="active" aria-current="true"></button>
             `);
         }else{
-            $("#carousel-indicators-info").append(
-            `
+            $("#carousel-indicators-info").append(`
                 <button type="button" data-bs-target="#carousel-bars-info" data-bs-slide-to="`+i+`" aria-label="Slide `+(i+1)+`"></button>
             `);
         }
@@ -102,55 +95,3 @@ $( document ).ready(function() {
     }
 });
 
-
-/*
-<div class="card my-5 mx-5" style="width:500px">
-        <div>
-          <div id="carouselBars" class="carousel slide">
-              <div class="carousel-indicators" id="carousel-indicators">
-                <button type="button" data-bs-target="#carouselBars" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselBars" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselBars" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#carouselBars" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#carouselBars" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                <button type="button" data-bs-target="#carouselBars" data-bs-slide-to="5" aria-label="Slide 6"></button>
-              </div>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/elephant.png" style="width:500px; height:500px" alt="elephant">
-                </div>
-                <div class="carousel-item active">
-                  <img src="" style="width:500px; height:500px" alt="willow">
-                </div>
-                  <div class="carousel-item active">
-                  <img src="https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/axelotl.png" style="width:500px; height:500px" alt="axelotl">
-                </div>
-                <div class="carousel-item active">
-                  <img src="" style="width:500px; height:500px" alt="cheetah">
-                </div>
-                  <div class="carousel-item active">
-                  <img src="https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/giraffe.png" style="width:500px; height:500px" alt="giraffe">
-                </div>
-                  <div class="carousel-item active">
-                  <img src="https://raw.githubusercontent.com/divs115/Contact-Card-Quiz/master/assets/cow.png" style="width:500px; height:500px" alt="cow">
-                </div>
-              </div>
-
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselBars" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselBars" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
-        </div>
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    
-    */
